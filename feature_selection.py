e04abc3c341f8c9c6419de752f1ef3ec98ad9a94
cday960 <c.day960@gmail.com>
2024-11-16 15:14:16 -0600

    updated

---
+++ feature_selection.py
@@ -0,0 +1,157 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.preprocessing import LabelEncoder
+from sklearn.feature_selection import mutual_info_classif, RFE
+from sklearn.ensemble import RandomForestClassifier
+
+
+def get_top_features_corr(attack_label, data, top_n=10):
+    # Select attack and normal flows
+    attack_data = data[data["Label"] == attack_label].select_dtypes(include=[np.number])
+    normal_data = data[data["Label"] == "NORMAL"].select_dtypes(include=[np.number])
+
+    # Combine normal and attack flows
+    combined = pd.concat([normal_data, attack_data])
+
+    # Create target column: 0 for NORMAL, 1 for specific attacks
+    combined["Target"] = [0] * len(normal_data) + [1] * len(attack_data)
+
+    # Calculate Pearson correlation
+    correlation_matrix = combined.corr()
+
+    # plt.figure(figsize=(30, 26))
+    # sns.heatmap(
+    #     correlation_matrix,
+    #     annot=False,
+    #     cmap="coolwarm",
+    #     cbar=True,
+    #     linewidths=0.3,
+    #     linecolor="gray",
+    #     xticklabels=True,
+    #     yticklabels=True,
+    # )
+    # plt.title("Feature to Feature interaction")
+    # plt.xticks(rotation=45, ha="right", fontsize=12)
+    # plt.yticks(fontsize=12)
+    # plt.tight_layout()
+    # # plt.show()
+    # plt.savefig("./figures/feature-to-feature.png", format="png", dpi=300)
+
+    # Get correlations with the target and sort by abs value
+    target_correlation = (
+        correlation_matrix["Target"].abs().sort_values(ascending=False).drop("Target")
+    )
+
+    # CSV stuff
+    # csv_data = target_correlation.head(top_n).reset_index()
+    # csv_data.columns = ["Feature", "Correlation"]
+    # csv_data.to_csv("./data/top_correlated_features.csv", index=False)
+
+    # Drop "Target" itself and return top N features
+    top_features = target_correlation.head(top_n)
+    bottom_features = target_correlation.tail(top_n)
+
+    # return target_correlation.drop("Target").head(top_n)
+    return [top_features.to_dict(), bottom_features.to_dict()]
+
+
+def get_top_features_mi(attack_label, data, top_n=10):
+    # Select attack and normal flows
+    attack_data = data[data["Label"] == attack_label].select_dtypes(include=[np.number])
+    normal_data = data[data["Label"] == "NORMAL"].select_dtypes(include=[np.number])
+
+    # Combine normal and attack flows
+    combined = pd.concat([normal_data, attack_data])
+    target = [0] * len(normal_data) + [1] * len(attack_data)
+
+    # Compute mutual information scores
+    mi_scores = mutual_info_classif(combined, target, discrete_features="auto")
+
+    # convert scores into dataframe
+    mi_scores_df = pd.Series(mi_scores, index=combined.columns).sort_values(
+        ascending=False
+    )
+
+    top = mi_scores_df.head(top_n)
+    bot = mi_scores_df.tail(top_n)
+    # print(type(top))
+
+    return [top, bot]
+
+
+def get_top_features_rf(attack_label, data, top_n=10):
+    # Select attack and normal flows
+    attack_data = data[data["Label"] == attack_label].select_dtypes(include=[np.number])
+    normal_data = data[data["Label"] == "NORMAL"].select_dtypes(include=[np.number])
+
+    # Combine normal and attack flows
+    combined = pd.concat([normal_data, attack_data])
+    target = [0] * len(normal_data) + [1] * len(attack_data)
+
+    # Fit random forest and get importances
+    rf = RandomForestClassifier(n_estimators=100, random_state=42)
+
+    rf.fit(combined, target)
+
+    feature_importances = pd.Series(
+        rf.feature_importances_, index=combined.columns
+    ).sort_values(ascending=False)
+
+    top = feature_importances.head(top_n)
+    bot = feature_importances.tail(top_n)
+
+    return [top, bot]
+
+
+def get_top_features_rfe(attack_label, data, n_features_to_select=10):
+    # Select attack and normal flows
+    attack_data = data[data["Label"] == attack_label].select_dtypes(include=[np.number])
+    normal_data = data[data["Label"] == "NORMAL"].select_dtypes(include=[np.number])
+
+    # Combine normal and attack flows
+    combined = pd.concat([normal_data, attack_data])
+    target = [0] * len(normal_data) + [1] * len(attack_data)
+
+    rf = RandomForestClassifier(n_estimators=100, random_state=42)
+
+    # Initialize rfe with num of feat to select
+    rfe = RFE(estimator=rf, n_features_to_select=n_features_to_select, step=1)
+
+    rfe.fit(combined, target)
+
+    selected_features = combined.columns[rfe.support_]
+    feature_ranking = pd.Series(rfe.ranking_, index=combined.columns).sort_values()
+
+    return selected_features, feature_ranking.head(10)
+
+
+def label_feature_correlation_heatmap(data):
+    binary_labels = pd.get_dummies(data["Label"])
+
+    numerical_data = data.select_dtypes(include=[np.number])
+
+    correlation_matrix = pd.concat([numerical_data, binary_labels], axis=1).corr()
+
+    label_correlation = correlation_matrix.loc[
+        numerical_data.columns, binary_labels.columns
+    ]
+
+    plt.figure(figsize=(30, 26))
+    sns.heatmap(
+        label_correlation,
+        annot=True,
+        cmap="coolwarm",
+        cbar=True,
+        linewidths=0.3,
+        linecolor="gray",
+    )
+
+    plt.title("Feature Label Correlation")
+    plt.xticks(rotation=45, ha="right", fontsize=14)
+    plt.yticks(fontsize=14)
+    plt.subplots_adjust(left=0.12)
+    plt.subplots_adjust(right=0.15)
+    plt.tight_layout()
+    plt.savefig("./figures/feature-to-label.png", format="png", dpi=300)
--- main.py
+++ main.py
@@ -1,20 +1,124 @@
-# import sklearn
+import sklearn
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.preprocessing import LabelEncoder
+from sklearn.feature_selection import mutual_info_classif, RFE
+from sklearn.ensemble import RandomForestClassifier
+from feature_selection import (
+    get_top_features_rfe,
+    get_top_features_corr,
+    get_top_features_rf,
+    get_top_features_mi,
+    label_feature_correlation_heatmap,
+)
 
 """
-- Each label has 199 entries.
+- Each label has 200 entries.
+- Use data.loc[data[<column header>] == <header value>]
+- Can extract specific features from above code using string indexing
 """
 
+
+def extract_feature(dataframe, label, feature):
+    result = []
+    for _, row in dataframe.iterrows():
+        if row["Label"] == label:
+            result.append(row[feature])
+    return result
+
+
+def average_feature(dataframe, label, feature):
+    total = 0
+    count = 0
+    for _, row in dataframe.iterrows():
+        if row["Label"] == label:
+            total += row[feature]
+            count += 1
+    return total / count
+
+
+def my_print(data):
+    for key, value in data.items():
+        print(f"{'-'*50}\n{key}:")
+        for x, y in value.items():
+            print(f"\t{x}: {y}")
+
+
+def histogram(data):
+    histo = {}
+    for value in data.values():
+        for label in value.keys():
+            if label not in histo:
+                histo[label] = 0
+            histo[label] += 1
+    return histo
+
+
+# Load data
 data = pd.read_csv("CICFlowMeter_Testing_Balanced.csv")
-labels = {}
 
-for label in data["Label"]:
-    if label not in labels:
-        labels[label] = {"Amount": 0}
-    else:
-        labels[label]["Amount"] += 1
-        # print(labels[label]["Amount"])
+# Features want to drop
+drop_features = [
+    "Unnamed: 0.1",
+    "Unnamed: 0",
+    "Src Port",
+]
+data = data.drop(columns=drop_features, errors="ignore")
+# print(data)
 
-# print(labels)
-print(data.head)
+# Encode labels to numerical value to make processing easier
+le = LabelEncoder()
+data["Encoded_Label"] = le.fit_transform(data["Label"])
+
+numerical_data = data.select_dtypes(include=[np.number])
+
+attack_labels = data["Label"].unique()
+attack_labels = [x for x in attack_labels if x != "NORMAL"]
+
+top_features_corr = {
+    label: get_top_features_corr(label, data, top_n=20)[0] for label in attack_labels
+}
+
+my_print(top_features_corr)
+
+top_features_mi = {
+    label: get_top_features_mi(label, data, top_n=20)[0] for label in attack_labels
+}
+my_print(top_features_mi)
+
+top_features_rf = {
+    label: get_top_features_rf(label, data, top_n=20)[0] for label in attack_labels
+}
+my_print(top_features_rf)
+
+
+rfe_results_by_attack = {
+    label: get_top_features_rfe(label, data, n_features_to_select=10)
+    for label in attack_labels
+}
+print(rfe_results_by_attack)
+
+# Creates and saves a correlation heatmap between labels and features
+# label_feature_correlation_heatmap(data)
+
+# Creates a csv for the top features correlation
+result = []
+
+for label in attack_labels:
+    top_features, _ = get_top_features_corr(label, data, top_n=20)
+
+    for feature, corr in top_features.items():
+        result.append({"Label": label, "Feature": feature, "Correlation": corr})
+
+results_df = pd.DataFrame(result)
+results_df.to_csv("./data/top_correlated_features.csv")
+
+
+# Creates a histogram
+# histo = histogram(top_features_corr)
+
+# print("\n\nFrequency of features:")
+# for key, value in histo.items():
+#     print(f"\t{key}: {value}")
